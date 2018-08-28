d5b6604aa77eb13ff5b51dbf874098db798d75c9
Luis Veiga <luis.veiga@fpfsistemas.local>
2018-08-28 18:13:10 +0100

    v1.0

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends AbstractController
+{
+    public function index(Request $request){
+    	//$request->request->get('campo');
+       return $this->render('base.html.twig');
+	}
+}
